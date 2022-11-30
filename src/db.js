3f0e22374aadbbbf6707c5ff02c58889f35418ae
Juan Segundo <juansepincha@gmail.com>
2022-11-29 22:47:36 -0300

    updating back

--- src/db.js
+++ src/db.js
@@ -17,6 +17,7 @@ const sequelize = new Sequelize(DB_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
